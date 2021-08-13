4ce72ded8e7a0a366262dedd4cbaa8657d1c0acc
Ramesh Reddy <60493999+ramesh181@users.noreply.github.com>
2021-08-13 08:05:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModel = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+const openModel = function() {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const closeModel = function() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+for(let i = 0; i < btnOpenModal.length; i++) {
+    btnOpenModal[i].addEventListener('click', openModel);
+}
+
+
+
+btnCloseModel.addEventListener('click', closeModel);
+overlay.addEventListener('click', closeModel);
+
+document.addEventListener('keydown', function(e) {
+
+
+    if(e.key === 'Escape' && !modal.classList.contains('hidden')){
+        closeModel();
+    }
+});
